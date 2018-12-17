f440e6a00b6d35c1b6ca952ad9fdf3f87fd08980
musicprogram <juan-o-henao@hotmail.es>
2018-12-17 17:53:35 -0500

    seeds, por fecha aleatoria y enum aleatorio con metodo de ruby

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+(4.months.ago.to_date..Date.current).each do |date|
+  rand(6).times do
+    Expense.create!(date: date, description: Faker::Commerce.product_name,
+        type: Expense.types.keys.sample, amount: Faker::Number.between(10, 500))
+  end
+end
