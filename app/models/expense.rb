ceac839dd590f2d3bbdf73fd6af6557ad8392501
musicprogram <juan-o-henao@hotmail.es>
2018-12-17 17:20:48 -0500

    iniciando annotate

---
+++ app/models/expense.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id          :integer          not null, primary key
+#  date        :date
+#  description :string
+#  type        :integer
+#  amount      :decimal(, )
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Expense < ApplicationRecord
+end
